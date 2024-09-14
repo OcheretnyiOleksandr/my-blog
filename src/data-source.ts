3b52c44fcdd99062d21a7ffbe60162b855ab8c77
o.ocheretnyi <oleksandr.ocheretnyi@pitchbook.com>
2024-09-14 14:15:37 +0300

    Added orm config and data source.

---
+++ src/data-source.ts
@@ -0,0 +1,15 @@
+import { DataSource } from 'typeorm';
+import { User } from './modules/user/entities/user.entity';
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'testUser',
+  password: 'testUser',
+  database: 'my_blog_db',
+  entities: [User],
+  migrations: ['dist/db/migrations/*.js'],
+  synchronize: false,
+  migrationsRun: true
+});
