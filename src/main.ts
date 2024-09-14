764025feeb6331dc66885ff2492b8273d692c91a
o.ocheretnyi <oleksandr.ocheretnyi@pitchbook.com>
2024-09-14 14:16:00 +0300

    Added swagger.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  swagger(app);
+
   await app.listen(3000);
 }
+
+const swagger = (app) => {
+  const config = new DocumentBuilder()
+    .setTitle('Blog')
+    .setDescription('Article Blog')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('swagger', app, document);
+};
+
 bootstrap();
