d9007865bc9241a9a3296d61f207cdfa7540ae0b
o.ocheretnyi <oleksandr.ocheretnyi@pitchbook.com>
2024-10-26 14:26:11 +0300

    Added like module to app module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { CommentModule } from './modules/comment/comment.module';
 import { AuthModule } from './modules/google/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { DbModule } from './config/db/db.module';
+import { LikeModule } from './modules/like/like.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { DbModule } from './config/db/db.module';
     UserModule,
     PostModule,
     CommentModule,
+    LikeModule,
   ],
 })
 export class AppModule {}
